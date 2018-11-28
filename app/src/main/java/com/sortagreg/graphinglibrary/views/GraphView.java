53da6dc5cf8806814fc9e28994cff699d5516722
Marshall Ladd <ladd.marshall@gmail.com>
2018-11-28 00:02:28 -0600

    Better documentation

--- app/src/main/java/com/sortagreg/graphinglibrary/views/GraphView.java
+++ app/src/main/java/com/sortagreg/graphinglibrary/views/GraphView.java
@@ -10,6 +10,11 @@ import android.view.View;
 
 import com.sortagreg.graphinglibrary.R;
 
+/**
+ * GraphView - Custom Graph View Class
+ *
+ * @author Marshall Ladd
+ */
 public class GraphView extends View {
     private Context context;
     private Paint backgroundPaint = new Paint();
@@ -24,12 +29,30 @@ public class GraphView extends View {
     private int numberOfVerticalMarkers = 5;
     private int numberOfHorizontalMarkers = 10;
 
+    /**
+     * Constructor for a GraphView in code.
+     *
+     * Will be used if the GraphView is created in code.
+     *
+     * @param context
+     */
     public GraphView(Context context) {
         super(context);
         this.context = context;
         setPaintLines();
+        init(null);
     }
 
+    /**
+     * Constructor for a GraphView in XML.
+     *
+     * Will be used if GraphView is created in an XML resource.
+     * Values that can be set in an XML file are made available
+     * by declaring them in the res/values/attrs.xml
+     *
+     * @param context
+     * @param attrs values from the XML set.
+     */
     public GraphView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
@@ -37,7 +60,14 @@ public class GraphView extends View {
         init(attrs);
     }
 
-    // initialize variables set in the XML
+    /**
+     * Method used to initialize all parameters on creation.
+     *
+     * Doing this during creation helps keep onDraw smaller
+     * which makes the View more optimized.
+     *
+     * @param attrs values from the XML set.
+     */
     private void init(@Nullable AttributeSet attrs) {
         if (attrs == null) return;
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.GraphView);
@@ -46,16 +76,33 @@ public class GraphView extends View {
         typedArray.recycle();
     }
 
-    public void setNumberOfVerticalMarkers(int newNumber) {
-        numberOfVerticalMarkers = newNumber;
+    /**
+     * Update the number of vertical cross markers are displayed in the GraphView
+     *
+     * @param numberOfVerticalMarkers int Number of vertical lines to display.
+     */
+    public void setNumberOfVerticalMarkers(int numberOfVerticalMarkers) {
+        this.numberOfVerticalMarkers = numberOfVerticalMarkers;
         invalidate();
     }
 
+    /**
+     * Update the number of vertical cross markers are displayed in the GraphView
+     *
+     * @param numberOfHorizontalMarkers int Number of vertical lines to display.
+     */
     public void setNumberOfHorizontalMarkers(int numberOfHorizontalMarkers) {
         this.numberOfHorizontalMarkers = numberOfHorizontalMarkers;
         invalidate();
     }
 
+    /**
+     * Overridden method that is called by the system.  This draws your View.
+     *
+     * Can be invoked by calling invalidate(), but this can be an expensive operation.
+     *
+     * @param canvas Canvas Object to be drawn to.
+     */
     @Override
     protected void onDraw(Canvas canvas) {
 //        super.onDraw(canvas);
@@ -64,6 +111,13 @@ public class GraphView extends View {
         drawHorizontalMarkers(canvas);
     }
 
+    /**
+     * Draws the vertical markers to the Canvas.
+     *
+     * Number of markers is configurable in Java and XML. DEFAULT: 5
+     *
+     * @param canvas Canvas Object to be drawn to
+     */
     private void drawVerticalMarkers(Canvas canvas) {
         // width of the data portion of the graph
         int graphWidth = canvas.getWidth() - leftAxisMargin - rightAxisMargin;
@@ -72,13 +126,20 @@ public class GraphView extends View {
         // print vertical markers
         for (int i = 1; i <= numberOfVerticalMarkers; i++) {
             int startX = leftAxisMargin + (i * markerSpacing);
-            int startY = topAxisMargin + 50; // add 100 to give some distance between axis and markers
+            int startY = topAxisMargin + 50; // add 50 to give some distance between axis and markers
             int endX = startX;
-            int endY = canvas.getHeight() - bottomAxisMargin - 50; // sub 100 for same reason add 100 earlier
+            int endY = canvas.getHeight() - bottomAxisMargin - 50; // sub 50 for same reason add 100 earlier
             canvas.drawLine(startX, startY, endX, endY, markerPaint);
         }
     }
 
+    /**
+     * Draws the horizontal markers to the Canvas.
+     *
+     * Number of markers is configurable in Java and XML. DEFAULT: 10
+     *
+     * @param canvas Canvas Object to be drawn to
+     */
     private void drawHorizontalMarkers(Canvas canvas) {
         int graphHeight = canvas.getHeight() - topAxisMargin - bottomAxisMargin;
         int markerSpacing = graphHeight / (numberOfHorizontalMarkers + 1);
@@ -91,17 +152,27 @@ public class GraphView extends View {
         }
     }
 
-    private void setPaintLines() {
-        axisPaint.setColor(0xff000000);
-        axisPaint.setStrokeWidth(8.0f);
-        markerPaint.setColor(0xffd3d3d3);
-        markerPaint.setStrokeWidth(3.0f);
-    }
-
+    /**
+     * Draws the border of the GraphView.
+     *
+     * TODO Make this configurable for either a 2 or 4 line border.
+     *
+     * @param canvas Canvas Object to be drawn to
+     */
     private void drawAxes(Canvas canvas) {
         // vertical axis T->B
         canvas.drawLine(leftAxisMargin, topAxisMargin, leftAxisMargin, canvas.getHeight() - bottomAxisMargin, axisPaint);
         // horizontal axis L->R
         canvas.drawLine(leftAxisMargin, canvas.getHeight() - bottomAxisMargin, canvas.getWidth() - rightAxisMargin, canvas.getHeight() - bottomAxisMargin, axisPaint);
+    }
+
+    /**
+     * Initialize the Paint Objects.
+     */
+    private void setPaintLines() {
+        axisPaint.setColor(0xff000000);
+        axisPaint.setStrokeWidth(8.0f);
+        markerPaint.setColor(0xffd3d3d3);
+        markerPaint.setStrokeWidth(3.0f);
     }
 }
